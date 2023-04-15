161bc29a8ea4b57ac5829e6614875e65e011e0f9
Lautaro Avalos <lautaro.avalos255@gmail.com>
2023-04-15 18:58:44 -0300

    Formulario-Contacto

--- RedSocial/astro.config.mjs
+++ RedSocial/astro.config.mjs
@@ -5,5 +5,13 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [react()]
+  integrations: [react()],
+  scripts: [
+    {
+      type: 'module',
+      src: '/_snowpack/pkg/jquery/dist/jquery.min.js',
+    },
+  ],
 });
+
+
